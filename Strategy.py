e19b8d9e1d9c314b7690bdff36a42fb8ada61163
NoName <53948477+8b1tz@users.noreply.github.com>
2021-09-25 19:15:00 -0300

    Update Strategy.py

--- Strategy.py
+++ Strategy.py
@@ -8,41 +8,46 @@ class Pessoa:
         self.peso = peso
         self.altura = altura
 
-class Comparacao(metaclass=ABCMeta):
+class StrategyComparacao(metaclass=ABCMeta):
 
     @abstractmethod
     def comparar(pessoa1, pessoa2):
         pass
 
 
-class StrategyComparePorIdade(Comparacao):
-
+class StrategyComparePorIdade(StrategyComparacao):
     def comparar(pessoa1, pessoa2):
         if(pessoa1.idade > pessoa2.idade):
             return f"{pessoa1.nome} é mais velha que {pessoa2.nome}"
         return f"{pessoa2.nome} é mais velha que {pessoa1.nome}"
 
-class StrategyComparePorAltura(Comparacao):
-
+class StrategyComparePorAltura(StrategyComparacao):
     def comparar(pessoa1, pessoa2):
         if (pessoa1.altura > pessoa2.altura):
             return f"{pessoa1.nome} é mais alta que {pessoa2.nome}"
         return f"{pessoa2.nome} é mais alta que {pessoa1.nome}"
 
-class StrategyComparePorPeso(Comparacao):
-
+class StrategyComparePorPeso(StrategyComparacao):
     def comparar(pessoa1, pessoa2):
         if (pessoa1.peso > pessoa2.peso):
             return f"{pessoa1.nome} é mais pesada que {pessoa2.nome}"
         return f"{pessoa2.nome} é mais pesada que {pessoa1.nome}"
 
+class GeradorComparacao:
+
+    def geradorComparacao(self, strategyComparacao):
+        self.strategyComparacao  = strategyComparacao
+
+    def gerarComparacao(pessoa1, pessoa2):
+        return strategyComparacao.comparar(pessoa1, pessoa2)
 
 def teste():
+    geradorComparacao = GeradorComparacao(StrategyComparacao(StrategyComparePorAltura))
+
     pessoaA = Pessoa("jessica", 20, 70, 1.80)
     pessoaB = Pessoa("Joao", 40, 60, 1.90)
 
-    print(StrategyComparePorAltura.comparar(pessoaA, pessoaB))
-    print(StrategyComparePorIdade.comparar(pessoaA,pessoaB))
-    print(StrategyComparePorPeso.comparar(pessoaA, pessoaB))
+    geradorComparacao.gerarComparacao(pessoaA, pessoaB)
+
 
 teste()
